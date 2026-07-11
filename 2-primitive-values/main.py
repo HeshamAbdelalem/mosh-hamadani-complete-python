ee793c6e81b2820432703da28ac53227861d7618
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2026-07-12 00:30:06 +0300

    2-primitive-values

---
+++ 2-primitive-values/main.py
@@ -0,0 +1,26 @@
+# we have 4 primitve values : strings, integer, float , boolean
+
+name = "Hesham"
+age = 37
+height = 1.9000
+study_python = True
+
+# print(name)
+# print(type(name))
+
+# print(age)
+# print(type(age))
+
+# print(height)
+# print(type(height))
+
+# print(study_python)
+# print(type(study_python))
+
+print(name[:5])
+
+first_name = "Mosh"
+last_name = "Hamedani"
+full = f"{first_name} {last_name}"
+
+print(full)
---
+++ 2-primitive-values/string_methods.py
@@ -0,0 +1,12 @@
+course = "Python Programming    "
+# everything in python is object , and every object have (functions) aka methods
+
+# print(course.strip())
+# print(course.upper())
+# print(course.capitalize())
+# print(course.find("o"))
+
+for i in course.strip():
+    print(i.upper())
+
+print("thon" not in course)
