143208e02ad37594dcbcf1f1b0845068d484d088
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2026-07-14 21:00:09 +0300

    control-flow-comparison-operators

---
+++ 3-control-flow/1-comparison_operator.py
@@ -0,0 +1,54 @@
+# state = 10 > 5
+
+# print(state)
+ 
+# a = 10 != 10
+
+# print(ord("H"))
+
+#? Conditional Statments
+
+# temperature = 20 
+
+# if temperature >= 30:
+#     print("It's Hot Today")
+#     print("Drink Water!!")
+# else:
+#     print("It's not Bad weather today!")
+#     print("don't forget water")
+    
+# print("Done!")
+
+#? Ternary Operator
+
+# userAge = 20
+
+# if userAge > 18:
+#     message = "You are allowed to enter"
+# else: 
+#     message = "You Are NOT Allowed to Enter!!!"
+    
+#! we can use instead ternary operator 
+#! value_if_true if condition else value_if_false
+
+# message = "You May Enter" if userAge > 18 else "You may NOT Enter"
+    
+# print(message)
+
+#? Logical Operators : and , or , not
+
+# good_credit = True
+# high_income = False
+# student = False
+
+# if not student and good_credit:
+#     print("not applicable")
+
+#? Chaining comparisor operators
+
+# age = 19
+
+# if age >= 18 and age < 60:  #here we can make it cleanin by chaining operators
+#     print("You may enter! WELCOME")
+# if 18 <= age < 60: 
+#     print("You may enter! WELCOME")
