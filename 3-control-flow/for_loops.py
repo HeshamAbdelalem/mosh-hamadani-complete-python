1a8c2bb240803c8ad2211e177adefd84e9f092a7
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2026-07-16 00:19:42 +0300

    while loop

---
+++ 3-control-flow/for_loops.py
@@ -0,0 +1,28 @@
+#? For Loops
+
+# for number in range(1,6):
+#     print("...." , number)
+    
+    
+# list_num = [64,987, 8,48,99,'asd']
+
+# for i in list_num:
+#     print(i)
+
+#? for else
+
+# success = False
+
+# for i in range(1,4):
+#     print(i, i * '.')
+#     if success:
+#         print("Done, Exit")
+#         break
+# else:
+#     print("else statement excuted")
+
+#? Nested loops
+
+for x in range(1,5):
+    for y in range(1,3):
+        print(f"({x} , {y})")
